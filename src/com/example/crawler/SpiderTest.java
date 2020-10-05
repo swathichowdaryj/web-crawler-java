79e2a26f9b20fb94b71dcb24fed80eaa292719f0
Kommina Swathi Mukeswari <swathim@sg.ibm.com>
2020-10-06 06:09:10 +0800

    code snippet to implement simple web crawler using java

---
+++ src/com/example/crawler/SpiderTest.java
@@ -0,0 +1,13 @@
+package com.example.crawler;
+
+public class SpiderTest {
+	
+	   
+	    public static void main(String[] args)
+	    {
+	        Spider spider = new Spider();
+	        spider.search("https://www.google.com/", "information");
+	    }
+	
+
+}
